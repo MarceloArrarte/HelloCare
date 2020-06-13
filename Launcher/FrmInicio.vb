e1aa290d61264aab0c67fc6a991154ec53358930
Marcelo Arrarte <hellocode.software@gmail.com>
2020-06-13 15:45:42 -0300

    Splash window
    Creación de ventana splash en el launcher con logos del producto y empresa.

---
+++ Launcher/FrmInicio.vb
@@ -0,0 +1,19 @@
+﻿Public Class FrmInicio
+    Dim appGestion As AplicacionGestion.FrmLogin
+    Dim appPacientes As AplicacionPacientes.FrmLogin
+    Private Sub FrmInicio_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        appGestion = New AplicacionGestion.FrmLogin
+        appPacientes = New AplicacionPacientes.FrmLogin
+
+        Me.CenterToScreen()
+        picHelloCare.Image = Image.FromFile("..\..\logo_hellocare.jpg")
+        picHelloCode.Image = Image.FromFile("..\..\logo_hellocode.jpg")
+    End Sub
+
+    Private Sub tmrSplashWindow_Tick(sender As Object, e As EventArgs) Handles tmrSplashWindow.Tick
+        tmrSplashWindow.Enabled = False
+        Me.Hide()
+        appGestion.Show()
+        appPacientes.Show()
+    End Sub
+End Class
--- Launcher/My Project/Application.Designer.vb
+++ Launcher/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Launcher.Form1
+            Me.MainForm = Global.Launcher.FrmInicio
         End Sub
     End Class
 End Namespace
